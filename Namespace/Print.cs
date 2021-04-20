b7bc232c64758c9000f384732a90c0fd9055546e
vamshi <vamshibondala888@gmail.com>
2021-04-20 11:32:10 +0530

    Namespaces

---
+++ Namespace/Print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using exNs;
+
+namespace ConsoleApp1
+{
+    class Print
+    {
+       public int Add(int a, int b)
+        {
+            System.Console.WriteLine(   "from consoleApp");
+            return a -b;
+        }
+        public void Just()
+        {
+            Console.WriteLine("in this we are printing value");
+
+        }
+    }
+}
---
+++ Namespace/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using exNs;
+using ConsoleApp1;
+
+namespace ConsoleApp1
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Arith a1 = new Arith();
+       
+            a1.Add(28, 22);
+            int substract=a1.Sub(20,10);
+            Console.WriteLine(" this is return value"+substract );
+           
+            
+            Print p = new Print();
+            p.Just();
+            p.Add(1,2);
+            
+        }
+    }
+}
+namespace exNs
+{
+    class Arith
+    {
+        public void Add(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine("this is the value"+c);
+        }
+        public int Sub(int a, int b)
+        {
+            int c = b - a;
+            return c;
+        }
+    }
+}
