decf757645c1e13f7a3e2606b5c54b3ddfde7244
vamshi <vamshibondala888@gmail.com>
2021-04-20 12:26:01 +0530

    TwoAndJogArray

---
+++ TwoDimenArray/Jogged/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DinamicJogged
+{
+    class DJog
+    {
+        public void VillNames()
+        {
+            Console.WriteLine("enter the row value for the array");
+           int val=int.Parse(Console.ReadLine());
+            string[][] vname = new string[][] 
+                             { new string[] { "1", "hello", "all", "4 " }, new string[]{"2","hello","all","4"}};
+            Console.WriteLine("enter the values into the array");
+            for (int r = 0; r < vname.Length; r++)
+            {
+               for (int c = 0; c < vname[r].Length; c++)
+               {
+                   vname[r][c] = Console.ReadLine();
+               }
+            }
+            Console.WriteLine(" the values of the array are ....");
+            for (int r = 0; r < vname.Length; r++)
+            {
+                for (int c = 0; c < vname[r].Length; c++)
+                {
+                    Console.WriteLine(vname[r][c]);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DJog d = new DJog();
+            d.VillNames();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TwoDimenArray/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoDimensionalArray
+{
+    class tar
+    {
+        public void Two()
+        {
+            int r=3, c=2;
+            int[,] two = new int[r,c];// { {1,2,3,4 },{3,2,1,4} };
+
+            //int[] one = new int[] { 2,21,23,43,54};
+            //for(int i = 0; i < two.Length; i++)
+            //{
+            //    Console.WriteLine(one[i]);
+
+            //}
+            two[0, 0] = 2;
+            two[0, 1] = 3;
+            two[0, 2] = 43;
+            two[0, 3] = 2;
+            two[1, 0] = 22;
+            two[1, 1] = 112;
+            two[1, 2] = 122;
+            two[1, 3] = 121;
+            two[2, 0] = 231;
+            two[2, 1] = 561;
+            two[2, 2] = 561;
+            two[2, 3] = 541;
+            two[2, 0] = 1;
+            //Console.WriteLine("enter the{0}x{1} values into the 2 dimensional array",two.GetLength(0),two.GetLength(1));
+            //for (int i= 0;i<two.GetLength(0); i++)
+            //{
+            //for (int j= 0;j<two.GetLength(1); j++)
+            //    {
+            //       two[i,j]= int.Parse(Console.ReadLine());
+            //    }
+            //}
+          
+            Console.WriteLine("array values are .....");
+            for (int i = 0; i < two.GetLength(0); i++)
+            {
+                for (int j = 0; j < two.GetLength(1); j++)
+                {
+                    Console.WriteLine((two[i, j])); 
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tar c1 = new tar();
+            c1.Two();
+        }
+    }
+}
