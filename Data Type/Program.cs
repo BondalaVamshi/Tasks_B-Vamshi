7ceb2898fd361916a0f59885f51cdc8d3b7c9813
vamshi <vamshibondala888@gmail.com>
2021-04-20 11:38:46 +0530

    Data Types

---
+++ Data Type/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataTypes1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string name = "rama";
+            name = "ramudu";
+            string hello = name == "ramudu" ? $"hello {name}!" : $"you are not {name}! ";
+           Console.WriteLine(hello);
+            //String name1 = "sita";
+            //Console.WriteLine("{0} , {1}",name,name1);
+            //StringBuilder nameOf = new StringBuilder();
+            //nameOf.Append("rama");
+            //nameOf.Append(" ramudu");
+            //Console.WriteLine(  nameOf);
+
+
+
+            //Console.BackgroundColor = ConsoleColor.Red;
+            //Console.ForegroundColor = ConsoleColor.Yellow;
+            //Console.WriteLine("this is for parsing");
+            //int i;
+            //i=int.Parse(Console.ReadLine());
+            //Console.WriteLine(i);
+
+            //Console.WriteLine("this is for parsing int");
+            //float i2=Convert.ToInt64(Console.ReadLine());
+            //Console.WriteLine(i2);
+            //Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Console.WriteLine($"size if datatype :{sizeof(float)}");
+
+            //Console.WriteLine("hello");
+            //Console.Write("hello");
+            //Console.Beep();
+            //Console.WriteLine("this is for int read");
+            //int testRead = Console.Read();
+            //Console.WriteLine("this is for int readline with parsing");
+            ////int testReadLineParsing = int.Parse(Console.ReadLine());
+            //Console.WriteLine("this is for int readline with convert ");
+            //int testRLConvert = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+            //int EID, SALARY;
+            //string ENAME, ADDRESS, JOB;
+            //Console.WriteLine("ENTER THE EMPLOYEE DTAILS");
+            //Console.WriteLine("ENTER THE EMPLOYEE ID");
+            //EID = int.Parse(Console.ReadLine());
+            //Console.WriteLine("ENTER THE EMPLOYEE NAME");
+            //ENAME = Console.ReadLine();
+            //Console.WriteLine("ENTER THE EMPLOYEE SALARY");
+            //SALARY = int.Parse(Console.ReadLine());
+            //Console.WriteLine("ENTER THE EMPLOYEE ADDRESS ");
+            //ADDRESS = Console.ReadLine();
+            //Console.WriteLine("ENTER THE EMPLOYEE JOB");
+            //JOB = Console.ReadLine();
+            //Console.WriteLine("\n\n\nTHE EMPLOYEE DETAILS ARE GIVEN BELOW :");
+            //Console.WriteLine("THE EMPLOYEE ID IS: " + EID);
+            //Console.WriteLine("THE EMPLOYEE NAME IS: " + ENAME);
+            //Console.WriteLine("THE EMPLOYEE SALARY IS: " + SALARY);
+            //Console.WriteLine("THE EMPLOYEE ADDRESS IS: " + ADDRESS);
+            //Console.WriteLine("THE EMPLOYEE JOB IS: " + JOB);
+            //Console.ReadKey();
+        }
+    }
+}
