99a95b0b3abadc827239e39d8921183a9a189960
vamshi <vamshibondala888@gmail.com>
2021-04-21 18:36:06 +0530

    Inheritence

---
+++ Interface/Interface/AnotherInterface/MultipleClassInheritence.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnotherInterface
+{
+    interface IA
+    {
+        void Name();
+    }
+    interface IB
+    {
+        void Location();
+    }
+    class CA:IA
+    {
+        public void Name()
+        {
+            Console.WriteLine("name of the  employee is \"Rani\"");
+        }
+    }
+
+    class CB:IB
+    {
+        public void Location()
+        {
+            Console.WriteLine("CB:name of the employee is \" Rajan\"");
+        }
+    }
+    class MultipleClassInheritence : IA,IB
+    {
+
+        CA ca = new CA();
+        CB cb = new CB();
+        public void Name()
+        {
+            Console.WriteLine("this is from multiple class inheritence");
+
+            ca.Name();
+        }
+        public void Location()
+        {
+            Console.WriteLine("this is from multiple class inheritence");
+
+            cb.Location();
+        }
+    }
+   
+}
---
+++ Interface/Interface/AnotherInterface/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AnotherInterface
+{
+    interface I1
+    {
+        public void name();
+    }
+    interface I2
+    {
+        public void name();
+    }
+    interface I3:I1,I2
+    {
+        //this is also possible
+    }
+
+    class Details:I1,I2
+    {
+       public  void name()
+        {
+            Console.WriteLine("this is default interface");
+        }
+        //this is explicit interface
+        //we call it with interface refernce variable  I2 
+        //(there involves type casting)  i.e,((I2)d).name();
+        //default means calling directly i.e, d.name();
+        void I2.name()
+        {
+            Console.WriteLine("this is vamshi");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Details d = new Details();
+            d.name();
+            ((I2)d).name();
+
+            MultipleClassInheritence MCI = new MultipleClassInheritence();
+            MCI.Name();
+            MCI.Location();
+        }
+    }
+}
