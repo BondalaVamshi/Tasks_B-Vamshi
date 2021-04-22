3db75b75d9988b03b5900eebb415ae4cb2123bb8
vamshi <vamshibondala888@gmail.com>
2021-04-22 14:55:23 +0530

    structures

---
+++ Structures/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Structures
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Structure1 s1;//= new Structure1();
+            ////s1.i;
+            //s1.msg(); this  case is  not possible because here no constructor to initalise the value
+
+            Structure1 s1 = new Structure1();
+            s1.msg();//which gives default values becaouse default constructor will assign values
+
+            Structure1 s2;
+            s2.i = 100;
+            s2.msg();
+
+            parametrs p = new parametrs();
+            p.msg2();
+        }
+    }
+}
---
+++ Structures/Structure1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Structures
+{
+    struct Structure1
+    {
+        public int i;
+        public void msg()
+        {
+            Console.WriteLine("the values from struct are " + i);
+        }
+    }
+
+    struct parametrs
+    {
+        private int _id;
+        public int ID
+        {
+            set { this._id = value; }
+            get { return this._id; }
+        }
+        public void msg2()
+        {
+            Console.WriteLine("enter the values imto the ID");
+            ID=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(ID);
+        }
+    }
+}
+
+    
+    
