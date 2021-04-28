fab9056cc5e0d370e55d92a41c2b9463538a4674
vamshi <vamshibondala888@gmail.com>
2021-04-28 19:30:26 +0530

    generics

---
+++ CSharper/Generics/EmpDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+   public class EmpDetails
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string job { get; set; }
+        public int Sal { get; set; }
+       
+    }
+}
